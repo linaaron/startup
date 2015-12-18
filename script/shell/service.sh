0f53fac9fca4bded3da9c50fa3c2ac7f61661c84
Aaron Lin <Aaron.Lin@ehealth-china.com>
2015-12-18 16:50:28 +0800

    new commit message

--- script/shell/service.sh
+++ script/shell/service.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
-function startTomcat(){
-    echo "Starting tomcat..."
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../
 
+function installWar() {
+    echo "Installing war..."
 
+    if [[ -z $1 ]]
+    then
+        mvn clean install  -Dmaven.test.skip=true
+    else
+        mvn clean install -Dmaven.test.skip=true
+    fi
+}
+
+function startTomcat() {
+    echo "Starting tomcat ..."
+    export CATALINA_HOME="$BASE_DIR/tomcat/one"
+    export CATALINA_BASE="$BASE_DIR/tomcat/one"
+    export JAVA_OPTS="-Dcatalina.home=$CATALINA_HOME -Dcatalina.base=$CATALINA_HOME  -Djava.io.tmpdir=$CATALINA_HOME\temp -ms256m -mx1024m  -XX:MaxPermSize=160m -Duser.language=en -Dfile.encoding=UTF8 -Duser.timezone=GMT -Djava.net.preferIPv4Stack=true"
+    export CATALINA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9991"
+    export CATALINA_PID="$BASE_DIR/tomcat/catalina.pid"
+
+    $CATALINA_HOME/bin/catalina.sh start
+}
+
+function stopTomcat() {
+    echo "Stoping tomcat ..."
+    export CATALINA_HOME="$BASE_DIR/tomcat/one"
+    export CATALINA_BASE="$BASE_DIR/tomcat/one"
+    export CATALINA_PID="$BASE_DIR/tomcat/catalina.pid"
+    $CATALINA_HOME/bin/catalina.sh stop -force
+}
+
+function startTomcatTwo(){
+    echo "Starting tomcat two..."
     export CATALINA_HOME="$BASE_DIR/software/apache-tomcat"
     export CATALINA_BASE="$BASE_DIR/main/target/tomcat"
     export JAVA_OPTS="-Dspring.profiles.active=prod -Dehi.images.root=$2 -Dext.props.path=$BASE_DIR/config/$1/environment -Dlog.level=INFO -Dlog.path=$BASE_DIR/logs -Duser.timezone=America/Los_Angeles -server -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m "
@@ -14,4 +44,4 @@ function startTomcat(){
     $CATALINA_HOME/bin/catalina.sh start
 }
 
-startTomcat
+#startTomcat
